947e872355f77042b943e5133833e7cb5c3d140b
Edward <e.arekhva@gmail.com>
2020-09-04 14:47:15 +0200

    add reducer

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case 'INC':
 			return state + 1;
+		case 'DEC':
+			return state - 1;
+		case 'RND':
+			return state - action.payload;
 
 		default:
 			return state;
@@ -13,8 +17,21 @@ const reducer = (state = initialState, action) => {
 };
 
 const store = createStore(reducer);
-store.subscribe(() => console.log(store.getState()));
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+const incButton = document.querySelector('#inc');
+const decButton = document.querySelector('#dec');
+const rndButton = document.querySelector('#rnd');
+const counter = document.querySelector('#counter');
+
+store.subscribe(() => (counter.textContent = store.getState()));
+
+incButton.addEventListener('click', () => {
+	store.dispatch({type: 'INC'});
+});
+decButton.addEventListener('click', () => {
+	store.dispatch({type: 'DEC'});
+});
+rndButton.addEventListener('click', () => {
+	const payload = Math.floor(Math.random() * 10);
+	store.dispatch({type: 'RND', payload});
+});
