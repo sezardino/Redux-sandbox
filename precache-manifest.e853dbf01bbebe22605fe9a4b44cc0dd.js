207f59e34e7738b1d8b2dee45df995bf1c2e029a
Edward <e.arekhva@gmail.com>
2020-09-06 08:29:36 +0200

    Updates

---
+++ precache-manifest.e853dbf01bbebe22605fe9a4b44cc0dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a134da534f4d2a44bdd0c9fce3da52d",
+    "url": "/Redux-sandbox/index.html"
+  },
+  {
+    "revision": "2177064e6a3b535ad1d3",
+    "url": "/Redux-sandbox/static/js/2.876201c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Redux-sandbox/static/js/2.876201c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aee940fd03c5bb7dfb4",
+    "url": "/Redux-sandbox/static/js/main.fcdbde93.chunk.js"
+  },
+  {
+    "revision": "4438fcbff7d67a36ecdd",
+    "url": "/Redux-sandbox/static/js/runtime-main.9b2b1ad1.js"
+  }
+]);
